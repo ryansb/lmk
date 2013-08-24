7759d5081412122bb520ce2d4dbea1c99680c08a
Ryan S. Brown <sb@ryansb.com>
2013-08-23 20:53:25 -0500

    Add ability to only notify on failure and to specify the subject separately.

--- lmk.go
+++ lmk.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"launchpad.net/goamz/aws"
 	"launchpad.net/goamz/exp/sns"
@@ -8,24 +9,51 @@ import (
 	"strings"
 )
 
-func main() {
-	msg := strings.Join(os.Args[1:], " ")
-	var conn *sns.SNS = sns.New(aws.Auth{AccessKeyId, AccessSecret}, aws.USEast)
+var onlyFailure int
+var specificSubject string
 
-	msgOpts := sns.PublishOpt{
+const nonFail int = -999
+
+func PublishMessage(msgOpts *sns.PublishOpt) {
+	var conn *sns.SNS = sns.New(aws.Auth{AccessKeyId, AccessSecret}, aws.USEast)
+	_, err := conn.Publish(msgOpts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	flag.IntVar(&onlyFailure, "only-failure", nonFail, "Only send "+
+		"notification if the prior command exited non-zero. Need to pass $? "+
+		" to this command.")
+	flag.StringVar(&specificSubject, "subject", "", "Specify the subject "+
+		"(defaults to beginning of message)")
+}
+
+func main() {
+	flag.Parse()
+
+	var msg string = strings.Join(flag.Args(), " ")
+
+	var msgOpts sns.PublishOpt = sns.PublishOpt{
 		Message:  msg,
 		Subject:  msg,
 		TopicArn: SnsTopicArn,
 	}
 
-	if len(msg) > 20 {
+	if specificSubject != "" {
+		msgOpts.Subject = specificSubject
+	} else if len(msg) > 20 {
 		msgOpts.Subject = msg[:20] + "..."
 	}
 
-	_, err := conn.Publish(&msgOpts)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if onlyFailure != nonFail {
+		if onlyFailure != 0 {
+			PublishMessage(&msgOpts)
+		}
 		return
+	} else {
+		PublishMessage(&msgOpts)
 	}
 }
